2ae43f477912660e14e9c08c54c20e8fd85bb88f
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-11-18 11:11:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,3 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 #AdguardHome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-#Mihomo
-git clone -b main --single-branch --filter=blob:none https://github.com/morytyann/OpenWrt-mihomo
-mv OpenWrt-mihomo/luci-app-mihomo package/
-mv OpenWrt-mihomo/mihomo package/
-#添加qosmate
-#git clone https://github.com/hudra0/qosmate.git package/qosmate
-#git clone https://github.com/LemonCrab666/luci-app-qosmate.git package/luci-app-qosmate
